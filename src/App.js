976fe3b3585270cd7fc0a327460993235eb6f514
Aniket Kshirsagar <aniketkshirsagar@12345678.local>
2023-10-16 23:24:27 +0530

    create pages and route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router , Routes,Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignOut from "./pages/SignOut";
+import Profile from "./pages/Profile";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
 
 function App() {
   return (
    <>
-    
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signOut" element={<SignOut />} />
+        <Route path="/Profile" element={<Profile />} />
+        <Route path="/ForgotPassword" element={<ForgotPassword />} />
+        <Route path="/Offers" element={<Offers /> } />
+      </Routes>
+    </Router>
    </>
   );
 }
